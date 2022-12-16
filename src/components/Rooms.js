7bfb89ef9c3f42779916a57779537b7412da2179
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-12-16 18:54:27 +0200

    added

--- src/components/Rooms.js
+++ src/components/Rooms.js
@@ -13,7 +13,7 @@ const Rooms = () => {
           </div>
           <h2 className='font-primary text-[45px] mb-4'>Rooms & Suites</h2>
         </div>
-        <div className='grid grid-cols-3 gap-[30px]'>
+        <div className='grid grid-cols-1 max-w-sm mx-auto gap-[30px] lg:grid-cols-3 lg:max-w-none lg:mx-0'>
           {rooms.map((room) => {
             return (
               <div
