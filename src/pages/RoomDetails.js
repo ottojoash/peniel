23e079cec481193e3faadef8974101c7231b1215
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-12-22 14:32:40 +0200

    added

--- src/pages/RoomDetails.js
+++ src/pages/RoomDetails.js
@@ -24,10 +24,26 @@ const RoomDetails = () => {
           {room.name} Details
         </h1>
       </div>
-      <div className='container mx-auto h-screen'>
-        <div className='flex flex-col lg:flex-row h-full'>
-          <div className='w-full h-full lg:w-[60%] bg-pink-200'>1</div>
-          <div className='w-full h-full lg:w-[40%] pt-12 px-6 bg-accent/20'>
+      <div className='container mx-auto'>
+        <div className='flex flex-col lg:flex-row h-full py-12'>
+          <div className='w-full h-full lg:w-[60%] px-6'>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci
+            architecto blanditiis labore neque. Harum, illo. Ea beatae quidem
+            voluptatibus aut accusantium, rerum similique quae excepturi
+            corporis dolor minus quibusdam aperiam sequi id voluptates, aliquid,
+            fugiat inventore ipsum temporibus tempora quasi. Ab recusandae
+            deserunt, eius quaerat sequi fugiat nesciunt eos repudiandae
+            similique aut tempora possimus consequatur commodi illo adipisci
+            exercitationem esse aspernatur ipsam voluptatem non ut sunt saepe
+            quia. Rerum expedita vel quaerat velit, cum autem ex ipsam deleniti
+            placeat sint illo ab harum quisquam pariatur quae in atque eaque!
+            Perspiciatis, quibusdam. Perferendis minus ducimus ex mollitia
+            minima laborum facere! Amet qui consequuntur eligendi ratione harum
+            commodi est nulla, mollitia quod dolore, praesentium laudantium
+            neque modi facere minima fuga dolorum quam molestiae rerum
+            molestias.
+          </div>
+          <div className='w-full h-full lg:w-[40%] py-12 px-6 bg-accent/20'>
             <div className='flex flex-col space-y-4 mb-4'>
               <div className='h-[60px]'>
                 <CheckIn />
