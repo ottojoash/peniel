3b65c962e1db2c46526234eabbbfbc64e58c3d7c
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-12-31 16:33:28 +0200

    added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className='bg-black py-12'>
+    <footer className='bg-primary py-12'>
       <div className='container mx-auto text-white text-center'>
         Copyright &copy; 2022. All rights reserved.
       </div>
