b07052be1e183c28e3cccfc6c885261f94d710ab
ottojoash <louisjoshbricks@gmail.com>
2024-03-07 08:20:50 +0300

    dashboard design Home check in and check out

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -2,9 +2,9 @@ import React, { useContext } from 'react';
 // components
 import AdultsDropdown from '../components/AdultsDropdown';
 import KidsDropdown from '../components/KidsDropdown';
-import CheckIn from '../components/CheckIn';
-import CheckOut from '../components/CheckOut';
 import { RoomContext } from '../context/RoomContext';
+import CheckInD from './CheckinD';
+import CheckoutD from './CheckoutD';
 
 const BookForm = () => {
   const { handleClick } = useContext(RoomContext);
@@ -12,10 +12,10 @@ const BookForm = () => {
     <form className='h-[300px] w-full lg:h-[70px]'>
       <div className='flex flex-col w-full h-full lg:flex-row'>
         <div className='flex-1 border-r'>
-          <CheckIn />
+          <CheckInD />
         </div>
         <div className='flex-1 border-r'>
-          <CheckOut />
+          <CheckoutD />
         </div>
         <div className='flex-1 border-r'>
           <AdultsDropdown />
